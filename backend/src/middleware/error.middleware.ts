347b95f49acc56bd45c13a62a481a0aaddf11491
upadhyayaman001718-commits <upadhyayaman001718@gmail.com>
2026-09-10 11:57:31 +0530

    Fix production build errors

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { Prisma } from "@prisma/client";
+import { Prisma } from "../generated/prisma/client";
 
 export const errorMiddleware = (
     err: any,
